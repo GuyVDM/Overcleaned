54963875d9c434c949a365a42fb59667d9ac0854
GuyvandermeulenDeltion <guy_meulen@live.nl>
2020-02-06 11:32:44 +0100

    Added servicelocator in works

---
+++ Overcleaned/Assets/Design-Pattern/Interfaces/IServiceLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IServiceLocator
+{
+
+    event Action OnAddedService;
+
+    event Action OnRemovedSerice;
+
+}
