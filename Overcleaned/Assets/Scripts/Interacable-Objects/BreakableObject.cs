21dd1fe8259b41389d470a91e8f86132ab41fdc8
GuyvandermeulenDeltion <guy_meulen@live.nl>
2020-02-24 10:36:27 +0100

    Breakable Test V_002

--- Overcleaned/Assets/Scripts/Interacable-Objects/BreakableObject.cs
+++ Overcleaned/Assets/Scripts/Interacable-Objects/BreakableObject.cs
@@ -79,7 +79,7 @@ public class BreakableObject : ToolInteractableObject, IPunObservable
     protected override void Awake() 
     {
         base.Awake();
-        repairProgressionUI = SpawnManager.SpawnObjectBasedOnConnectionState("RepairingProgressBar", Vector3.zero, Quaternion.identity).GetComponentInChildren<ProgressBar>();
+        repairProgressionUI = Instantiate(Resources.Load("RepairingProgressBar") as GameObject, Vector3.zero, Quaternion.identity).GetComponentInChildren<ProgressBar>();
         repairProgressionUI.Set_LocalPositionOfPrefabRootTransform(transform, object_ui_Offset);
         repairProgressionUI.Set_ActionName(repairActionName);
         repairProgressionUI.Set_Tooltip(repairActionTooltip);
