be5f8bb6d45809cfd4175abd145c13a85ea0e24c
GuyvandermeulenDeltion <guy_meulen@live.nl>
2020-03-06 10:14:15 +0100

    Added layermask and editted cleaning objects

--- Overcleaned/Assets/Scripts/Interacable-Objects/WieldableCleanableObject.cs
+++ Overcleaned/Assets/Scripts/Interacable-Objects/WieldableCleanableObject.cs
@@ -17,23 +17,70 @@ public class WieldableCleanableObject : WieldableObject
     [SerializeField]
     private MeshRenderer cleaned_Variant;
 
+    [Header("Debug Settings:")]
+    public bool isCleaned = false;
+
     #region ### RPC Calls ###
     [PunRPC]
     protected override void Stream_OnInteractionComplete() 
     {
         CleanObject();
     }
+
+    [PunRPC]
+    private void Stream_OnDirtyObject() 
+    {
+        DirtyObject();
+    }
     #endregion
+
+    #region ### Private Variables ###
+    private const int dirty_LayerMask = 1 << 10;
+
+    private int starting_ToolID;
+    #endregion
+
+    private void Awake() => starting_ToolID = cleanedToolID;
 
     public void CleanObject() 
     {
         toolID = cleanedToolID;
+        isCleaned = true;
         Debug.LogWarning("[WieldableCleanableObject] This script still needs to assign penalty on instance, and remove when this function is called.");
 
         if (uncleaned_Variant && cleaned_Variant) 
         {
             uncleaned_Variant.enabled = false;
             cleaned_Variant.enabled = true;
+        }
+    }
+
+    public void DirtyObject() 
+    {
+        toolID = starting_ToolID;
+        isCleaned = false;
+
+        if (uncleaned_Variant && cleaned_Variant) 
+        {
+            uncleaned_Variant.enabled = true;
+            cleaned_Variant.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.gameObject.layer == dirty_LayerMask) 
+        {
+            if(isCleaned == true) 
+            {
+                if(NetworkManager.IsConnectedAndInRoom) 
+                {
+                    photonView.RPC(nameof(Stream_OnDirtyObject), RpcTarget.AllBuffered);
+                    return;
+                }
+
+                Stream_OnDirtyObject();
+            }
         }
     }
 }
