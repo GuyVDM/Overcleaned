eec30c8317962753b38f4c2d3f23e1293c4b178f
Stephan Holding <stephanholding@gmail.com>
2020-04-20 13:04:53 +0200

    Dropdown UI art added

---
+++ Overcleaned/Assets/Scripts/UI/UI_MoreGraphicReferences.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class UI_MoreGraphicReferences : MonoBehaviour
+{
+	public enum UIType
+	{
+		Toggle,
+
+	}
+
+	public UIType uiType;
+	public Image[] imagesToToggle;
+
+	private void Awake()
+	{
+		AssignToUIElement();
+	}
+
+	private void AssignToUIElement()
+	{
+		switch (uiType)
+		{
+			case UIType.Toggle:
+				Toggle t = GetComponent<Toggle>();
+				t.onValueChanged.AddListener(delegate { ValueChanged(t.isOn); });
+				break;
+		}
+	}
+
+	private void ValueChanged(bool isOn)
+	{
+		ToggleImages(isOn);
+	}
+
+	private void ToggleImages(bool isOn)
+	{
+		for (int i = 0; i < imagesToToggle.Length; i++)
+		{
+			imagesToToggle[i].enabled = isOn;
+		}
+	}
+
+}
