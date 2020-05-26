4b2c2a8d6c9ba36859dd74f4e07e32fc192a4264
GuyVanDerMeulen <guy_meulen@live.nl>
2020-05-26 12:17:22 +0200

    Added cars and music

---
+++ Overcleaned/Assets/Scripts/Car.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public float CarSpeed { get; set; }
+    public float CarDrivingDuration { get; set; }
+
+    #region ### Private Variables ###
+    private Vector3 startPos;
+    private Vector3 startRotation;
+
+    private bool canMove = false;
+    #endregion
+
+    private void Start() 
+    {
+        startPos = transform.position;
+    }
+
+    public void SetSpeedAndDuration(float speed, float duration) 
+    {
+        CarSpeed = speed;
+        CarDrivingDuration = duration;
+    }
+
+    private void Update() 
+    {
+        if(canMove) 
+        {
+            transform.position += transform.forward * (CarSpeed * Time.deltaTime);
+        }
+    }
+
+    public void StartCar() 
+    {
+        if (canMove == false) 
+        {
+            canMove = true;
+            StartCoroutine(TempCarLoop());
+        }
+    }
+
+    private IEnumerator TempCarLoop() 
+    {
+        yield return new WaitForSeconds(CarDrivingDuration);
+
+        StopCar();
+    }
+
+    public void StopCar() 
+    {
+        canMove = false;
+        transform.position = startPos;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.root.GetComponent<Photon.Pun.PhotonView>()) 
+        {
+            if (col.transform.root.GetComponent<Photon.Pun.PhotonView>().IsMine)
+            {
+                if (col.transform.GetComponent<PlayerController>()) 
+                {
+                    col.transform.GetComponent<PlayerController>().StunPlayer(5);
+                }
+            }
+        }
+    }
+}
---
+++ Overcleaned/Assets/Scripts/CarManager.cs
@@ -0,0 +1,47 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct CarBehaviour 
+    {
+        public Car car;
+        public float carSpeed;
+        public float carDuration;
+    }
+
+    [Header("Car Settings:")]
+    [SerializeField]
+    private CarBehaviour[] allCars;
+
+    private void Start() 
+    {
+        HouseManager.OnFinishedCountdown += StartLoop;
+
+        for(int i = 0; i < allCars.Length; i++) 
+        {
+            allCars[i].car.SetSpeedAndDuration(allCars[i].carSpeed, allCars[i].carDuration);
+        }
+    }
+
+    public void StartLoop() 
+    {
+        StartCoroutine(CarLoop());
+    }
+
+    private IEnumerator CarLoop() 
+    {
+        const float TIME_BASE = 5;
+
+        while(true) 
+        {
+            for (int i = 0; i < allCars.Length; i++) 
+            {
+                yield return new WaitForSeconds(TIME_BASE);
+                allCars[i].car.StartCar();
+            }
+        }
+    }
+}
